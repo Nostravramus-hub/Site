36382191bca4fcd99357403a91b1f0239afea6f8
Nostravramus-hub <82176358+Nostravramus-hub@users.noreply.github.com>
2022-05-22 12:15:48 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost","id16535304_root","=v7=[Ng=S!Sq\_Ej","id16535304_signup");
+    $con = mysqli_connect("localhost","root","","signup");
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
